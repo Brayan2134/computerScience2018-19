ad9bc92e1fc8ebcd2d0537305d3b38041937ddfd
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2018-11-30 16:06:17 -0800

    Basic Sequence - Setup.
    I have everything setup, now I just need to connect the procedures.

--- Pascal/Projects/MENU/MENUS.pas
+++ Pascal/Projects/MENU/MENUS.pas
@@ -18,6 +18,69 @@ program programname;
 
 {Libs}
 uses crt;
+
+ {Intro to program}
+ procedure intro;
+  begin
+   writeln('Welocome to the AIRLINE CATALOGE':65);
+   writeln;
+   writeln('In this program, you can explore the different airlines.');
+   writeln('Let''s begin!');
+  end;
+ {End intro}
+
+ {Display menu}
+  procedure menu(var tempMenuSelection: char);
+   begin
+    writeln;
+    writeln('Please Pick A Region':10);
+    writeln;
+    writeln('[N]orth America.');
+    writeln('[S]outh America.');
+    writeln('[E]urope.');
+    writeln('[A]sia.');
+    writeln('A[F]rica.');
+    write('Please make a selection: ');
+    readln(tempMenuSelection);
+    tempMenuSelection := upcase(tempMenuSelection);
+   end;
+ {End display menu}
+
+ {Authenticate Selection}
+  procedure authSelection(var tempSelect: char);
+   begin
+    // Check to see if user typed a valid response
+    if (tempSelect = 'N') or (tempSelect = 'S') or (tempSelect = 'E') or (tempSelect = 'A') or (tempSelect = 'F') then
+     begin // DON'T TOUCH
+     end // DON'T TOUCH
+     else // If User types invalid char then...
+      repeat // ... Repeat loop until user types valid response.
+       begin
+        writeln('Invalid Option, Please Try Again.');
+        write('Please make another selection: ');
+        readln(tempSelect);
+        tempSelect := upcase(tempSelect);
+        end;
+       until (tempSelect = 'N') or (tempSelect = 'S') or (tempSelect = 'E') or (tempSelect = 'A') or (tempSelect = 'F');
+   end;
+ {End Authenticate Selection}
+
+ {Outro}
+  procedure outro;
+   begin
+    writeln('Thank you for using this program');
+   end;
+ {End Outro}
 {******************************************************************************}
+var
+ menuSelection: char;
+
 begin {main}
+ intro; // Intro squenence
+ menu(menuSelection); // Display Menu
+ authSelection(menuSelection);// Authticate
+
+ // Outro
+ outro;
+ readkey;
 end. {main}
