6171335349bbca25a17b749712daa1c7e7b15fc3
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2018-12-06 11:44:27 -0800

    Added method of counting vowels.

--- Pascal/Projects/TEXTMANI/TEXTMANI.pas
+++ Pascal/Projects/TEXTMANI/TEXTMANI.pas
@@ -36,6 +36,7 @@ uses crt;
 var {main}
  name: string;
  i: integer;
+ vowels: integer;
 
 begin {main}
 
@@ -43,13 +44,38 @@ begin {main}
  writeln('Please input a string upto 80 Chars.');
  readln(name);
 
- //Output the string one letter at a time.
+ // Output the string one letter at a time.
+ {
  for i := 1 to 80 do
  begin
   write(name[i]);
   delay(90);
   textcolor(random(16));
  end;
+ }
+
+ // Upcase entire string.
+ {
+ for i := 1 to 80 do
+ begin
+  name[i] := upcase(name[i]);
+ end;
+ writeln(name);
+ }
+
+ // Count vowels in a phrase.
+ vowels := 0;
+ name := upcase(name);
+
+ for i := 1 to 80 do
+ begin
+  if (name[i] in ['A', 'E', 'I', 'O', 'U']) then // Check each letter/number against vowels
+  begin // Vowels
+   vowels := vowels + 1;
+  end; // End Vowels
+ end;
+ writeln(name, ' contains ', vowels, ' vowels.');
+
 
  // End of program.
  writeln;
