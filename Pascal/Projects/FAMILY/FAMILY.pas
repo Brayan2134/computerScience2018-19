79d9f6116edc83e78b6055584a07abdef370d94a
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2018-12-08 22:05:11 -0800

    Display Menu Is Done.
    Now that we can display the menu, we have to make the user input information and authenticate.

--- Pascal/Projects/FAMILY/FAMILY.pas
+++ Pascal/Projects/FAMILY/FAMILY.pas
@@ -25,19 +25,42 @@ uses
  {Introduction}
  procedure intro;
  begin
+  delay(200);
   writeln('THE FAMILY YOU NEVER HAD?!!?':65);
   writeln;
+  delay(200);
   writeln('Welcome to the family program!');
   writeln;
+  delay(200);
   writeln('In this progam, you can input or read any data in your family!');
   writeln;
+  delay(200);
   writeln('While deletion of data is not available in this build,');
   writeln;
+  delay(200);
   writeln('It will be coming soon!');
+  writeln('********************************************************************');
  end;
  {End Introduction}
+
+ {Menu}
+ procedure menu;
+ begin
+  writeln;
+  textbackground(green);
+   writeln('Please select an option:');
+  textbackgroud(black);
+  writeln('[I]nput new family member.');
+  writeln('[R]eview family history.');
+  writeln('[Q]uit.');
+ end;
+ {End Menu}
 {******************************************************************************}
 begin
+ textcolor(white); // Sets uniform color for entire program
+
  intro; // Introduction
+
+
  readkey;
 end.
