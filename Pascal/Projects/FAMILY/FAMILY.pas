62e2d20fc80e00976eda80dec80af4d20d18211f
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2018-12-08 21:52:58 -0800

    Family With Arrays.
    This is a new project for my computer science class starting of the date, december 8, 2018. The purpose of this program is to learn how to use arrays, call them, and minipulate data with them.
    The program will consist of these parts:
    - The user gets an introduction to the program
    - The user can choose to either quit/go to the array (where any data is stored) or make a new entry.
    - Should work in any order.

---
+++ Pascal/Projects/FAMILY/FAMILY.pas
@@ -0,0 +1,18 @@
+{
+ NAME: Brayan Quevedo Ramos
+ CLASS: Computer Science 1
+ DATE: 12/8/18
+
+ PURPOSE: This is a program where
+          the user can input, grab, and display any data that
+          they have entered.
+
+ STRUCTURE: - Intro
+            - Menu
+             - Show Data
+             - Input Data
+             - Quit
+            - Outro
+
+ REMARKS: -none-
+}
