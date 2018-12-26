ba20c2d2994358379e801af2e3df8de3c239d1e9
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2018-12-25 18:36:19 -0800

    Program Done...
    This is the live and running version of my project. While very basic, I intend to do this again, with dynamic arrays and make this look very polished.

--- Pascal/Projects/FAMILY/FAMILY.pas
+++ Pascal/Projects/FAMILY/FAMILY.pas
@@ -27,8 +27,20 @@ uses
   writeln;
   textbackground(blue);
    delay(300);
-   writeln('Press any button to continue.');
+   writeln('Press the ENTER button to continue.');
+   readln;
   textbackground(black);
+ end;
+
+ // The outro for the program
+ procedure outro;
+ begin
+ writeln;
+ delay(500);
+ writeln('THANK YOU for using this program!');
+ delay(500);
+ textbackground(blue);
+ writeln('PLEASE press any key to end the program.');
  end;
 
 {******************************************************************************}
@@ -49,19 +61,52 @@ begin
  // Gather data for array
  intro; // Introduction into program
  clrscr; // Clear the screen after user has seen the intro
+ writeln('DATA GATHERING.':65);
 
  // First individual data for array
+ writeln;
  textbackground(green);
-  writeln('Please enter the name of the first individual:');
+  delay(300);
+  write('Please enter the name of the first individual: ');
+  textbackground(blue);
+  readln(name0);
  textbackground(black);
 
  // Second individual data for array
+ writeln;
+ textbackground(green);
+  delay(300);
+  write('Please enter the name of the second individual: ');
+  textbackground(blue);
+   readln(name1);
+ textbackground(black);
 
  // Third individual data for array
+ writeln;
+ textbackground(green);
+  delay(300);
+  write('Please enter the name of the third individual: ');
+  textbackground(blue);
+  readln(name2);
+ textbackground(black);
 
  // Fourth individual data for array
+ writeln;
+ textbackground(green);
+  delay(300);
+  write('Please enter the name of the fourth individual: ');
+  textbackground(blue);
+  readln(name3);
+ textbackground(black);
 
  // Fifth individual data for array
+ writeln;
+ textbackground(green);
+  delay(300);
+  write('Please enter the name of the last individual: ');
+  textbackground(blue);
+  readln(name4);
+ textbackground(black);
 
  // Sort gathered data from variables and store into array
  names_list[0] := name0;
@@ -71,17 +116,28 @@ begin
  names_list[4] := name4;
 
  // Output the data from array
+ clrscr;
+ writeln('THE RESULTS OF THE PROGRAM.':65);
+ writeln;
+ textbackground(red);
+ delay(500);
  writeln('The name of the first family member is: ',names_list[0]);
  writeln;
+ delay(500);
  writeln('The name of the second family member is: ',names_list[1]);
  writeln;
+ delay(500);
  writeln('The name of the third family member is: ',names_list[2]);
  writeln;
+ delay(500);
  writeln('The name of the fourth family member is: ',names_list[3]);
  writeln;
+ delay(500);
  writeln('The name of the last family member is: ',names_list[4]);
+ textbackground(black);
 
  // Conclusion of program
+ outro;
 
  readkey;
 end.
